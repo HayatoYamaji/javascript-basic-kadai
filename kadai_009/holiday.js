45fea202cea831e52717781eb99f6e6b30199dad
HayatoYamaji <69965629+HayatoYamaji@users.noreply.github.com>
2023-09-29 23:10:23 +0900

    kadai_009をアップロード

---
+++ kadai_009/holiday.js
@@ -0,0 +1,17 @@
+// 変数の宣言
+const holidays = ['正月','成人の日','建国記念日','天皇誕生日','春分の日','昭和の日','憲法記念日','みどりの日','こどもの日','海の日','山の日','敬老の日','秋分の日','スポーツの日','文化の日','勤労感謝の日'];
+
+// for構文
+for (let any of holidays) {
+  console.log (any);
+}
+
+let index = 0;
+
+// while構文
+// holidays
+while (index < holidays.length) {
+  console.log(holidays[index]);
+  index ++;
+}
+
